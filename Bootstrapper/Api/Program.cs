64005b57217d3f57839617e431cdcf0d2c5be75d
Showkat Akbar mithu <showkat131080@gmail.com>
2025-02-28 19:40:20 +0600

    Add DI with module and api

--- Bootstrapper/Api/Program.cs
+++ Bootstrapper/Api/Program.cs
@@ -1,5 +1,16 @@
+using Basket;
+using Catalog;
+using Ordering;
 var builder = WebApplication.CreateBuilder(args);
+
+//services
+
+builder.Services.AddCatalogModule(builder.Configuration);
+builder.Services.AddBasketModule(builder.Configuration);
+builder.Services.AddOrderingModule(builder.Configuration);
 var app = builder.Build();
+
+//
 
 app.MapGet("/", () => "Hello World!");
 
