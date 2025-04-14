4642c878f8fbebd5fa4d5e747c019db16ef2d558
Showkat Akbar mithu <showkat131080@gmail.com>
2025-04-14 23:44:15 +0600

    add db context and product migration and seed data using reuseable code update migratin and seed

--- Modules/Catalog/CatalogModule.cs
+++ Modules/Catalog/CatalogModule.cs
@@ -1,8 +1,10 @@
-﻿using Catalog.Data;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Catalog.Data.Seed;
+using Shared.Data.Seed;
 namespace Catalog
 {
     public static class CatalogModule
@@ -11,17 +13,28 @@ namespace Catalog
         {
 
             // Data - Infrastructure services
-            var connectionString = configuration.GetConnectionString("Database");
-            services.AddDbContext<CatalogDbContext>((sp, options) =>
-            {
-                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
-                options.UseNpgsql(connectionString);
-            });
+
+            services.AddDbContext<CatalogDbContext>(option =>
+            option.UseSqlServer(configuration.GetConnectionString("Database")));
+            //var connectionString = configuration.GetConnectionString("Database");
+            //services.AddDbContext<CatalogDbContext>((sp, options) =>
+            //{
+            //    options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            //    options.UseNpgsql(connectionString);
+            //});
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            // 1. Use Api Endpoint services
+
+            // 2. Use Application Use Case services
+
+            // 3. Use Data - Infrastructure services  
+            app.UseMigration<CatalogDbContext>();
             return app;
         }
     }
---
+++ Modules/Catalog/Data/Migrations/20250414154148_initalMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "catalog");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "catalog",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ImageFile = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "catalog");
+        }
+    }
+}
