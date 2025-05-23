e74d79df10683fe827ab4acc277fc7523411f344
Showkat Akbar mithu <showkat131080@gmail.com>
2025-05-23 15:43:35 +0600

    basket domain and dbContext

--- Modules/Basket/BasketModule.cs
+++ Modules/Basket/BasketModule.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data;
+using Shared.Data.Interceptors;
 namespace Basket
 {
     public static class BasketModule
     {
         public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
         {
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<BasketDbContext>((sp, option) =>
+            {
+
+                option.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                option.UseSqlServer(configuration.GetConnectionString("Database"));
+            });
             return services;
         }
         public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
         {
+            // 1. Use Api Endpoint services
+
+            // 2. Use Application Use Case services
+
+            // 3. Use Data - Infrastructure services  
+            app.UseMigration<BasketDbContext>();
             return app;
         }
     }
